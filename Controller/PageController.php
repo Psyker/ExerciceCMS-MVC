46007287658b05439c8cd5e4a0b1b6866a68033b
Psyker <theo.bourgoin@hetic.net>
2016-05-13 09:40:59 +0200

    Add PageController, init composer, PageRepository

---
+++ Controller/PageController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bourgoin
+ * Date: 13/05/2016
+ * Time: 09:24
+ */
+
+namespace Controller;
+
+
+class PageController
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function addAction()
+    {
+
+    }
+
+    public function displayAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+    public function listAction()
+    {
+
+    }
+
+    public function detailsAction()
+    {
+
+    }
+
+
+
+
+
+
+}
---
+++ Model/PageRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bourgoin
+ * Date: 13/05/2016
+ * Time: 09:39
+ */
+
+namespace Repository;
+
+
+class PageRepository
+{
+
+}
